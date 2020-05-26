2f1eaa5c2bf62b6ed9368154184ca4c901f9f438
Kirill Shukhin <kirillshux@gmail.com>
2020-05-26 22:49:44 +0300

    v 0.0.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello!!!")
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { LikeButton } from "./components/LikeButton.js";
+
+// es modules
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById("container");
+    ReactDOM.render(React.createElement(LikeButton), container);
+});
